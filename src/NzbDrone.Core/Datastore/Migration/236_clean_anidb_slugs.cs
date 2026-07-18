144b6925caf00eac5bfd51a0deb5ec887cd1f749
jt-ito <jtbjr07@gmail.com>
2026-07-18 11:54:25 -0600

    fix: postgresql migration syntax and remove discord webhooks

--- src/NzbDrone.Core/Datastore/Migration/236_clean_anidb_slugs.cs
+++ src/NzbDrone.Core/Datastore/Migration/236_clean_anidb_slugs.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Data;
 using FluentMigrator;
 using NzbDrone.Core.Datastore.Migration.Framework;
 
@@ -8,11 +10,51 @@ namespace NzbDrone.Core.Datastore.Migration
     {
         protected override void MainDbUpgrade()
         {
-            Execute.Sql(@"
-                UPDATE OR IGNORE Series
-                SET TitleSlug = SUBSTR(TitleSlug, 1, INSTR(TitleSlug, '-anidb-') - 1)
-                WHERE INSTR(TitleSlug, '-anidb-') > 0;
-            ");
+            Execute.WithConnection(CleanAniDbSlugs);
+        }
+
+        private void CleanAniDbSlugs(IDbConnection conn, IDbTransaction tran)
+        {
+            using (var getSeriesCmd = conn.CreateCommand())
+            {
+                getSeriesCmd.Transaction = tran;
+                getSeriesCmd.CommandText = "SELECT \"Id\", \"TitleSlug\" FROM \"Series\" WHERE \"TitleSlug\" LIKE '%-anidb-%'";
+                using (var seriesReader = getSeriesCmd.ExecuteReader())
+                {
+                    while (seriesReader.Read())
+                    {
+                        var id = seriesReader.GetInt32(0);
+                        var titleSlug = seriesReader.GetString(1);
+
+                        var index = titleSlug.IndexOf("-anidb-", StringComparison.Ordinal);
+                        if (index <= 0) continue;
+
+                        var newSlug = titleSlug.Substring(0, index);
+
+                        using (var checkCmd = conn.CreateCommand())
+                        {
+                            checkCmd.Transaction = tran;
+                            checkCmd.CommandText = "SELECT COUNT(1) FROM \"Series\" WHERE \"TitleSlug\" = ?";
+                            checkCmd.AddParameter(newSlug);
+                            
+                            var exists = Convert.ToInt32(checkCmd.ExecuteScalar()) > 0;
+                            
+                            if (!exists)
+                            {
+                                using (var updateCmd = conn.CreateCommand())
+                                {
+                                    updateCmd.Transaction = tran;
+                                    updateCmd.CommandText = "UPDATE \"Series\" SET \"TitleSlug\" = ? WHERE \"Id\" = ?";
+                                    updateCmd.AddParameter(newSlug);
+                                    updateCmd.AddParameter(id);
+        
+                                    updateCmd.ExecuteNonQuery();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
