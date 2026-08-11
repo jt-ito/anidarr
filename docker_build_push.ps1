80f96959992b0a8b8649db050b5fe183b2b5ca63
jt-ito <jtbjr07@gmail.com>
2026-08-11 10:35:34 -0600

    chore: bump package version to v10.0.0 and update docker script to use backend version

--- docker_build_push.ps1
+++ docker_build_push.ps1
@@ -1,8 +1,19 @@
 Set-Location $PSScriptRoot
-Write-Host "Building Docker image for jteaito/anidarr:latest..."
-docker build -t jteaito/anidarr:latest .
 
-Write-Host "Pushing Docker image to Docker Hub..."
+$buildInfoPath = Join-Path $PSScriptRoot "src\NzbDrone.Common\EnvironmentInfo\BuildInfo.cs"
+$buildInfoContent = Get-Content $buildInfoPath -Raw
+if ($buildInfoContent -match 'new Version\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)') {
+    $version = "$($matches[1]).$($matches[2]).$($matches[3]).$($matches[4])"
+} else {
+    Write-Error "Could not parse version from BuildInfo.cs"
+    exit 1
+}
+
+Write-Host "Building Docker image for jteaito/anidarr:$version and latest..."
+docker build -t jteaito/anidarr:$version -t jteaito/anidarr:latest .
+
+Write-Host "Pushing Docker images to Docker Hub..."
+docker push jteaito/anidarr:$version
 docker push jteaito/anidarr:latest
 
 Write-Host "Done!"
